6af5e3b2a90c1424606255454fd6187ad5a46dac
WadaKota <wada@impv.co.jp>
2025-04-28 14:06:42 +0900

    chore: Prettier の導入

--- geo-narrator/eslint.config.js
+++ geo-narrator/eslint.config.js
@@ -1,19 +1,26 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js, prettier: pluginPrettier },
+    extends: ['js/recommended', 'plugin:prettier/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{jsx,tsx}"], // 対象をJSXを使うファイルだけにする。
+    files: ['**/*.{jsx,tsx}'], // 対象をJSXを使うファイルだけにする。
     rules: {
-      "react/react-in-jsx-scope": "off",
-    }
-  }
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]);
